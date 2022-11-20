2d76fdc56ab5a59af88e35feb1a14a7761c464e3
Yazan <zkac357@live.rhul.ac.uk>
2022-11-20 22:17:39 +0000

    Changes:     - Resized pane to make it more compact     - Re-arranged containers/controls to fit the new pane
        - Added Calculate button which takes input from the 'expression' text box
          - Doubles and outputs any numeric input into the 'result' text box
          - Copies any string input into the 'result' text box

--- src/main/java/uk/ac/rhul/cs2800/MainView.java
+++ src/main/java/uk/ac/rhul/cs2800/MainView.java
@@ -1,11 +1,16 @@
 package uk.ac.rhul.cs2800;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 public class MainView {
+
+    @FXML
+    private Button calculateButton;
 
     @FXML
     private TextField expressionField;
@@ -21,7 +26,18 @@ public class MainView {
 
     @FXML
     private Label resultLabel;
+    
+    @FXML
+    void calculateIsPressed(ActionEvent event) {
+    	try {
+    		// If expression is a float value, double it and output it
+    		Float input = 2 * Float.parseFloat(expressionField.getText());
+    		resultField.setText(String.valueOf(input));
+    		
+    	} catch (Exception e) {
+    		// otherwise copy the contents of expression over to result
+    		resultField.setText(expressionField.getText());
+    	}
+    }
 
 }
-
-
