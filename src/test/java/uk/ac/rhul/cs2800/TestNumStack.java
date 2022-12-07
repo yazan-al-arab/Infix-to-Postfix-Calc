accf9d5c64f6281aa980b0ea7cbc5bb196572932
Yazan <zkac357@live.rhul.ac.uk>
2022-12-07 16:24:39 +0000

    NumStack TDD: Edited an existing test ('push()'), and it worked off the bat.
    No more tests were needed as the remaining functionality depends on the Stack class tests.

--- src/test/java/uk/ac/rhul/cs2800/TestNumStack.java
+++ src/test/java/uk/ac/rhul/cs2800/TestNumStack.java
@@ -5,42 +5,46 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestNumStack {
-  
+
   private NumStack numStack;
-  
+
   @BeforeEach
   void setUp() {
     numStack = new NumStack();
   }
-  
+
   @Test
   void isEmpty() {
     assertEquals(numStack.isEmpty(), true, "The numstack should be empty.");
   }
-  
+
   @Test
   void push() {
     numStack.push(5.0f);
     assertEquals(numStack.isEmpty(), false, "The numstack shouldn't be empty because a value was pushed to it.");
   }
-  
+
   @Test
   void pop() {
     numStack.push(5.0f);
     assertEquals(numStack.pop(), 5.0f, "The return value should equal 5.0 because that is what was initially.");
+    assertEquals(numStack.isEmpty(), true, "The numstack shouldn't be empty because a value was pushed to it.");
   }
-  
+
   @Test
   void pushMany() {
-    for(float i=0; i<5; i++) {  
+    for (float i = 0; i < 5; i++) {
       numStack.push(i);
     }
     assertEquals(numStack.isEmpty(), false, "The numstack shouldn't be empty because many values was pushed to it.");
-    
-    for(float i=4; i>=0; i--) {
+
+    for (float i = 4; i >= 0; i--) {
       assertEquals(numStack.pop(), i, "The popped value is not correct");
     }
     assertEquals(numStack.isEmpty(), true, "The numstack should now be empty after popping all it's values.");
   }
+
+  // There are no more tests required as all the remaining functionality relies on
+  // the Stack class.
 
 }
