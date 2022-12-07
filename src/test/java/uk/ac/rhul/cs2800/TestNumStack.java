c37985e21c6a8f2db0d8a37f1e12386a88c080b3
Yazan <zkac357@live.rhul.ac.uk>
2022-12-07 14:31:16 +0000

    TDD: Created a TestNumStack class, hence also a NumStack class with a constructor.

---
+++ src/test/java/uk/ac/rhul/cs2800/TestNumStack.java
@@ -0,0 +1,14 @@
+package uk.ac.rhul.cs2800;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class TestNumStack {
+  
+  private NumStack numStack;
+  
+  @BeforeEach
+  void setUp() {
+    numStack = new NumStack();
+  }
+
+}
