4ee43e6eddaa4a613fde229e9e3a67f230ee7336
Yazan <zkac357@live.rhul.ac.uk>
2022-12-08 18:49:50 +0000

    StrStack TDD: pushManyThenPop() test was created. It is the final test and it covers pushing and popping in different orders.
    The remaining functionality depends on the Stack class, therefore no more
    tests are needed.

--- src/test/java/uk/ac/rhul/cs2800/TestStrStack.java
+++ src/test/java/uk/ac/rhul/cs2800/TestStrStack.java
@@ -31,5 +31,24 @@ class TestStrStack {
     assertEquals(strStack.pop(), "test", "The return value should equal \"test\" because that is what was initially.");
     assertEquals(strStack.isEmpty(), true, "The strstack should be empty because all the values were popped.");
   }
+  
+  @Test
+  void pushManyThenPopMany() {
+    strStack.push("Hello");
+    strStack.push("there.");
+    
+    assertEquals(strStack.pop(), "there.", "The return value should equal \"test\" because that is the most recent push.");
+    
+    strStack.push("How are you?");
+    
+    assertEquals(strStack.pop(), "How are you?", "The return value should equal \"How are you?\" because that is the most recent push. It maybe didn't pop properly.");
+    assertEquals(strStack.isEmpty(), false, "The stack shouldn't be empty. It has 1 item left inside.");
+    
+    assertEquals(strStack.pop(), "Hello", "The return value should equal \"Hello\" because that is what was initially pushed.");
+    assertEquals(strStack.isEmpty(), true, "The stack should be empty. All the items should've been popped.");
+  }
+
+  // There are no more tests required as all the remaining functionality relies on
+  // the Stack class.
 
 }
