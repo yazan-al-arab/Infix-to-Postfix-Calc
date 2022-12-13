17229d56f0c8e8062ad9e3b6c4d42124c5d452d1
Yazan <zkac357@live.rhul.ac.uk>
2022-12-13 20:09:58 +0000

    RevPolishCalc TDD: Created a RevPolishCalc and TestRevPolishCalc class.
    I also created the RevPolishCalc's constructor by declaring a private
    field in the test class.

---
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -0,0 +1,13 @@
+package uk.ac.rhul.cs2800;
+
+public class RevPolishCalc implements Calculator {
+
+  public RevPolishCalc() {
+  }
+
+  @Override
+  public float evaluate(String what) throws InvalidExpression {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+}
---
+++ src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
@@ -0,0 +1,22 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestRevPolishCalc {
+  
+  private Calculator calc;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    calc = new RevPolishCalc();
+  }
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
