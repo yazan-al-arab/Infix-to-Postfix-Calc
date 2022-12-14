6eb9cfa96debf2a1baf247619e0c1b1d02ba89e9
Yazan <zkac357@live.rhul.ac.uk>
2022-12-14 21:05:10 +0000

    MVC MainView: Configured observers and all other methods.
    What I've done:
    1. Moved driver start code to here
    
    2. Connected the observer to notify if calculate button is pressed
    
    3. Used Dave's getInstance code to create a singleton

--- src/main/java/uk/ac/rhul/cs2800/MainView.java
+++ src/main/java/uk/ac/rhul/cs2800/MainView.java
@@ -1,12 +1,15 @@
 package uk.ac.rhul.cs2800;
 
-import javafx.event.ActionEvent;
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * The main stage where the whole calculator exists.
@@ -14,7 +17,7 @@ import javafx.scene.layout.Pane;
  * @author yazan
  *
  */
-public class MainView {
+public class MainView extends Application implements ViewInterface {
 
   @FXML
   private Button calculateButton;
@@ -40,24 +43,66 @@ public class MainView {
   @FXML
   private RadioButton infixRadio;
 
-
+//  /**
+//   * Calculates and outputs the expression.
+//   * 
+//   * @param event calculate button pressed
+//   */
+//  @FXML
+//  public void calculateIsPressed(ActionEvent event) {
+//    try {
+//      // If expression is a float value, double it and output it
+//      Float input = 2 * Float.parseFloat(expressionField.getText());
+//      resultField.setText(String.valueOf(input));
+//
+//    } catch (Exception e) {
+//      // otherwise copy the contents of expression over to result
+//      resultField.setText(expressionField.getText());
+//    }
+//  }
   
-  /**
-   * Calculates and outputs the expression.
-   * 
-   * @param event calculate button pressed
-   */
+  private volatile static MainView instance = null;
+  
   @FXML
-  public void calculateIsPressed(ActionEvent event) {
-    try {
-      // If expression is a float value, double it and output it
-      Float input = 2 * Float.parseFloat(expressionField.getText());
-      resultField.setText(String.valueOf(input));
-
-    } catch (Exception e) {
-      // otherwise copy the contents of expression over to result
-      resultField.setText(expressionField.getText());
+  void initialize() {
+    instance = this;
+  }
+  
+  // Inspired from Dave's code
+  public synchronized static MainView getInstance() {
+    if (instance == null) {
+      new Thread(() -> Application.launch(MainView.class)).start();
+      // Wait until the instance is ready since initialize has executed.
+      while (instance == null) {// empty body
+      }
     }
+    return instance;
+  }
+
+  @Override
+  public void addCalculateObserver(Observer f) {
+    calculateButton.setOnAction(event -> f.notifyObservers());
+  }
+
+  @Override
+  public String getExpression() {
+    return expressionField.getText();
+  }
+
+  @Override
+  public void menu() {
+    calculateButton.setDisable(false);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Pane pane = FXMLLoader.load(getClass().getClassLoader().getResource("MainView.fxml"));
+    Scene scene = new Scene(pane);
+
+    primaryStage.setTitle("CS2800 Calculator");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
   }
 
 }
